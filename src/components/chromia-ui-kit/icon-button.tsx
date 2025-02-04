d685bc9e7671f9766294cda88fe21d648d3c6e06
Natnael-arch <natnaelb8@gmail.com>
2025-02-04 02:24:31 -0800

    Update remote repository with latest project

--- src/components/chromia-ui-kit/icon-button.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { IconButton } from "@chromia/ui-kit";
-
-export default IconButton as React.FC<
-  React.ComponentProps<typeof IconButton> & {
-    variant?: string;
-  } & React.ButtonHTMLAttributes<HTMLButtonElement>
->;
--- src/components/chromia-ui-kit/input.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { Input } from "@chromia/ui-kit";
-
-export default Input as React.FC<
-  React.ComponentProps<typeof Input> &
-    React.InputHTMLAttributes<HTMLInputElement>
->;
